c7563da84d1490456447e7584ce5a5e87d125b6a
meliam <meli452@gmail.com>
2020-02-01 22:39:32 -0300

    Commit inicial al 20200201

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import TarjetaFruta from './componentes/TarjetaFruta'
+
+const App = () => (
+    <div>
+        <TarjetaFruta name='Sandia' price={5000} />
+        <TarjetaFruta name='Naranja' price={3500} />
+        <TarjetaFruta name='Kiwi' price={36500} />
+    </div>
+)
+
+export default App
---
+++ src/componentes/TarjetaFruta.js
@@ -0,0 +1,46 @@
+import React from 'react'
+import './TarjetaFruta.css'
+
+class TarjetaFruta extends React.Component {
+    state ={
+            cantidad: 0
+    }
+    
+    agregar = () => {
+        this.setState ({cantidad: this.state.cantidad + 1})
+    }
+
+    quitar = () => {
+        this.setState ({cantidad: this.state.cantidad - 1})
+    }
+    
+    limpiar = () => {
+        this.setState ({cantidad: 0})
+    }
+
+    render () {
+ 
+        return (<div className='TarjetaFruta'>
+            <h3>{this.props.name}</h3>
+            <hr />
+            <div> Cantidad: {this.state.cantidad}</div>
+            <button 
+                onClick={this.agregar}
+            >+</button>
+            <button 
+                onClick={this.quitar}
+            >-</button>
+            <button 
+                onClick={this.limpiar}
+            >Limpiar</button>
+            <p>$ {this.props.price}</p>
+            <p>
+                Total: ${this.props.price * this.state.cantidad}
+            </p>
+    
+        </div>)    
+    }
+}
+
+
+export default TarjetaFruta
